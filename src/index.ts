bfcbdbf7d55470016fd9781998b37e0061560b9d
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-09-22 18:14:10 -0400

    feat: add improved upload protocol

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import fs from "fs";
 
 // @ts-ignore
-import { STORE_PATH } from "@dignetwork/dig-sdk";
 import { startPropagationServer } from "./server";
 import tasks from "./tasks";
-
-if (!fs.existsSync(STORE_PATH)) {
-  fs.mkdirSync(STORE_PATH, { recursive: true });
-}
 
 tasks.start();
 startPropagationServer();
